b55a86952b06dd5558961e9c43d2e71974fa1ae3
Piotr Kulpinski <piotr@kulpinski.pl>
2024-04-12 15:27:27 +0200

    clean up index file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-export type WithOptional<Type, Key extends keyof Type> = Pick<Partial<Type>, Key> & Omit<Type, Key>
+// External exports
+export * from "scule"
 
-export type WithRequired<Type, Key extends keyof Type> = Type & {
-  [Prop in Key]-?: Type[Prop]
-}
+// Internal exports
+export * from "./colors/colors"
+export * from "./errors/errors"
+export * from "./events/events"
+export * from "./format/format"
+export * from "./helpers/helpers"
+export * from "./http/http"
+export * from "./numbers/numbers"
+export * from "./objects/objects"
+export * from "./params/params"
+export * from "./parsers/parsers"
+export * from "./random/random"
+export * from "./time/time"
+
+export type WithOptional<Type, Key extends keyof Type> = Pick<Partial<Type>, Key> & Omit<Type, Key>
+export type WithRequired<Type, Key extends keyof Type> = Type & { [Prop in Key]-?: Type[Prop] }
 
 export type DeepIdx<T, K extends string> = K extends ""
   ? T
@@ -23,20 +37,3 @@ export type ValidatePath<T, K extends string> = K extends ""
         ? `${K0}.${ValidatePath<T[K0], KR>}`
         : Extract<keyof T, string>
       : Extract<keyof T, string>
-
-// External exports
-export * from "scule"
-
-// Internal exports
-export * from "./colors/colors"
-export * from "./errors/errors"
-export * from "./events/events"
-export * from "./format/format"
-export * from "./helpers/helpers"
-export * from "./http/http"
-export * from "./numbers/numbers"
-export * from "./objects/objects"
-export * from "./params/params"
-export * from "./parsers/parsers"
-export * from "./random/random"
-export * from "./time/time"
